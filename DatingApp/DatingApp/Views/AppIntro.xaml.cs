da857be4bebff31494a4909f6857c3375d10bb70
vishnu <vishnudevxamarin.gmail.com>
2021-03-17 19:09:48 +0530

    1. Added image to background screen 2.added App intro screen.

---
+++ DatingApp/DatingApp/Views/AppIntro.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DatingApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AppIntro : ContentPage
+    {
+        public AppIntro()
+        {
+            InitializeComponent();
+        }
+
+        private void GotoHomeScreen(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new HomeScreen());
+        }
+    }
+}
