ec6d32e7ced72485a93213215de8fb88ec9d16eb
vishnu <vishnudevxamarin.gmail.com>
2021-03-18 19:22:32 +0530

    Added signin page and navigation

--- DatingApp/DatingApp/Views/ForgotPassword.xaml.cs
+++ DatingApp/DatingApp/Views/ForgotPassword.xaml.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -6,16 +10,11 @@ using Xamarin.Forms.Xaml;
 namespace DatingApp.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class AppIntro : ContentPage
+    public partial class ForgotPassword : ContentPage
     {
-        public AppIntro()
+        public ForgotPassword()
         {
             InitializeComponent();
-        }
-
-        private void GotoHomeScreen(object sender, EventArgs e)
-        {
-            Navigation.PushAsync(new LoginPage());
         }
     }
 }
--- DatingApp/DatingApp/Views/SignUpScreen.xaml.cs
+++ DatingApp/DatingApp/Views/SignUpScreen.xaml.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -6,16 +10,11 @@ using Xamarin.Forms.Xaml;
 namespace DatingApp.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class AppIntro : ContentPage
+    public partial class SignUpScreen : ContentPage
     {
-        public AppIntro()
+        public SignUpScreen()
         {
             InitializeComponent();
-        }
-
-        private void GotoHomeScreen(object sender, EventArgs e)
-        {
-            Navigation.PushAsync(new LoginPage());
         }
     }
 }
