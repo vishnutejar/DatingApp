580ed8febec43cd063704a1933be8433cb28deea
vishnu <vishnudevxamarin.gmail.com>
2021-03-18 16:39:26 +0530

    added fonts and styles in app.xaml

--- DatingApp/DatingApp/AssemblyInfo.cs
+++ DatingApp/DatingApp/AssemblyInfo.cs
@@ -1,3 +1,7 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("SourceSansPro-Bold.ttf", Alias = "SansBold")]
+[assembly: ExportFont("SourceSansPro-Light.ttf", Alias = "SansLight")]
+[assembly: ExportFont("SourceSansPro-Regular", Alias = "SansRegular")]
