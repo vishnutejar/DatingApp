d9d5c4ca82903714b261e7c09404bf6c80be5981
vishnu <vishnudevxamarin.gmail.com>
2021-03-25 20:58:27 +0530

    added circler image design to image in liked page

--- DatingApp/DatingApp/Views/HomePages/LikedPersonsInformation.xaml.cs
+++ DatingApp/DatingApp/Views/HomePages/LikedPersonsInformation.xaml.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -12,9 +8,32 @@ namespace DatingApp.Views.HomePages
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class LikedPersonsInformation : ContentPage
     {
+        ObservableCollection<string> lstOflikedPersons { get; set; } = new ObservableCollection<string>();
+        ObservableCollection<string> lstOfmessage { get; set; } = new ObservableCollection<string>();
         public LikedPersonsInformation()
         {
             InitializeComponent();
+
+            lstOflikedPersons.Add("person1.jpg");//new Student(){Name="",Marks="70"}
+            lstOflikedPersons.Add("person2.jpg");
+            lstOflikedPersons.Add("person3.jpg");
+            lstOflikedPersons.Add("person4.jpg");
+            lstOflikedPersons.Add("person5.jpg");
+            lstOflikedPersons.Add("person4.jpg");
+            lstOflikedPersons.Add("person2.jpg");
+
+
+            lstOfmessage.Add("person1.jpg");//new Student(){Name="",Marks="70"}
+            lstOfmessage.Add("person2.jpg");
+            lstOfmessage.Add("person3.jpg");
+            lstOfmessage.Add("person4.jpg");
+            lstOfmessage.Add("person5.jpg");
+            lstOfmessage.Add("person4.jpg");
+            lstOfmessage.Add("person2.jpg");
+
+
+            hrlstlikedperons.ItemsSource = lstOflikedPersons;
+            vrlistmessage.ItemsSource = lstOfmessage;
         }
     }
 }
