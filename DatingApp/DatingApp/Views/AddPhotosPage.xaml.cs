982346e1a078c900d424670faba67b37368d8046
vishnu <vishnudevxamarin.gmail.com>
2021-03-25 18:11:41 +0530

    Added code for cammera and gallery photo pick option in add photo screen

--- DatingApp/DatingApp/Views/AddPhotosPage.xaml.cs
+++ DatingApp/DatingApp/Views/AddPhotosPage.xaml.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,6 +13,7 @@ namespace DatingApp.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AddPhotosPage : ContentPage
     {
+        string PhotoPath;
         public AddPhotosPage()
         {
             InitializeComponent();
@@ -21,5 +23,59 @@ namespace DatingApp.Views
         {
             Navigation.PopAsync();
         }
+        async Task TakePhotoAsync()
+        {
+            try
+            {
+                var photo = await MediaPicker.CapturePhotoAsync();
+                await LoadPhotoAsync(photo);
+                Console.WriteLine($"CapturePhotoAsync COMPLETED: {PhotoPath}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"CapturePhotoAsync THREW: {ex.Message}");
+            }
+        }
+         async Task SelectPhotoAsync()
+        {
+            try
+            {
+                var photo = await MediaPicker.PickPhotoAsync();
+                await LoadPhotoAsync(photo);
+                Console.WriteLine($"CapturePhotoAsync COMPLETED: {PhotoPath}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"CapturePhotoAsync THREW: {ex.Message}");
+            }
+        }
+
+        async Task LoadPhotoAsync(FileResult photo)
+        {
+            // canceled
+            if (photo == null)
+            {
+                PhotoPath = null;
+                return;
+            }
+            // save the file into local storage
+            var newFile = Path.Combine(FileSystem.CacheDirectory, photo.FileName);
+            using (var stream = await photo.OpenReadAsync())
+            using (var newStream = File.OpenWrite(newFile))
+                await stream.CopyToAsync(newStream);
+
+            PhotoPath = newFile;
+        }
+
+        private async Task SelectImageFromGallery(object sender, EventArgs e)
+        {
+           await SelectPhotoAsync();
+        }
+
+        private async Task PhotoCaptureFromCamera(object sender, EventArgs e)
+        {
+            await TakePhotoAsync();
+        }
+
     }
 }
